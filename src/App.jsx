462e599bd06824c47678fd3090f63bc5cdf8f41c
Kristijan Karanfiloski <kristijan.karanfiloski@gmail.com>
2024-01-18 15:14:49 +0100

    'izbrisani nepotrebni imports'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
-import CallbackFunctions from "./components/callback_functions/CallbackFunctions";
-import DestructuringJs from "./components/destructuring_JS/DestructuringJs";
 import FunctionsJS from "./components/functions_JS/FunctionsJS";
-import MapMethod from "./components/map_method/MapMethod";
 
 function App() {
   return (
@@ -15,3 +12,4 @@ function App() {
   );
 }
 export default App;
+// TODO:  fuck this
